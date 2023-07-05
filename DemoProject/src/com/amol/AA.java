1a723d33e59417c51a011db70e271c8a7b772d57
JAYA VYAWHARE <JAYA VYAWHARE@DESKTOP-6G9M66L>
2023-07-05 11:25:46 +0530

    my first project commit

---
+++ DemoProject/src/com/amol/AA.java
@@ -0,0 +1,16 @@
+package com.amol;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AA {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.setProperty("webdriver.chrome.driver", "D://chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+
+	}
+
+}
---
+++ DemoProject/src/com/amol/TestLogger.java
@@ -0,0 +1,34 @@
+package com.amol;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestLogger {
+	
+/*
+		public static WebDriver driver;
+		public static Logger log;
+		public static void main(String[] args) {
+
+			log= LogManager.getLogger(TestLogger.class);
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get("https://www.myntra.com/");
+			log.info("Logged into Myntra");
+			try {
+				boolean text= driver.findElement(By.xpath("//a[contains(text(),'Women')]")).isDisplayed();
+			}
+			catch(Exception e) {
+				System.out.print(e.getMessage());
+				log.error("Exception occured", new Exception("Element Not Found"));
+			}
+			finally {
+				driver.quit();
+				log.info("Quitting the driver");
+			}
+			
+			
+		}
+	*/
+}
