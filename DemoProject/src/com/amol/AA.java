5028849d6aac85ee5ca7cfd50127983a5f638880
Amol2802 <138649028+Amol2802@users.noreply.github.com>
2023-07-10 09:44:56 +0530

    Update AA.java
    code from git repository

--- DemoProject/src/com/amol/AA.java
+++ DemoProject/src/com/amol/AA.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AA {
 
 	public static void main(String[] args) {
+		System.out.println("code from git repo");
 		System.out.println("Hello world123");
 		System.out.println("Hello world");
 		System.setProperty("webdriver.chrome.driver", "D://chromedriver.exe");
